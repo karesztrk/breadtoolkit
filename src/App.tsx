7653f7f8a68c8ae5b25d5450d6fafebd1ba73e9f
Károly Török <torok.karoly.krisztian@gmail.com>
2020-11-04 17:39:36 +0100

    Chakra initial import

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import React, { useState } from 'react';
 import './App.css';
-import './bootstrap.min.css';
-import { Container, Button, Row, Col } from 'react-bootstrap';
+import {
+  ThemeProvider,
+  theme,
+  CSSReset,
+  Button,
+  Heading,
+  Container,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+} from '@chakra-ui/core';
 
 const App = () => {
   const [flour, setFlour] = useState(500);
   return (
-    <Container>
-      <Row>
-        <Col>
-          {flour}gramm
-          <Button onClick={() => setFlour((prevState) => prevState + 1)}>
-            +
-          </Button>
-          <Button onClick={() => setFlour((prevState) => prevState - 1)}>
-            -
-          </Button>
-        </Col>
-      </Row>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Container>
+        <Heading as="h1" size="2xl" mb="2">
+          I just consumed some ⚡️Chakra!
+        </Heading>
+        {flour}gram
+        <Button onClick={() => setFlour((prevState) => prevState - 1)}>
+          -
+        </Button>
+        <Button onClick={() => setFlour((prevState) => prevState + 1)}>
+          +
+        </Button>
+        <NumberInput defaultValue={500} min={1} step={1}>
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </Container>
+    </ThemeProvider>
   );
 };
 
