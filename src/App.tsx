2afb39ec0e1d6717e42337ff39e6c8a45d166c9d
Károly Török <torok.karoly.krisztian@gmail.com>
2020-11-25 16:03:43 +0100

    SEO improvement

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '@app/App.css';
 import '@app/fonts.css';
 import { ChakraProvider } from '@chakra-ui/core';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Meta from '@app/components/layout/Meta';
 import theme from '@app/theme';
 import Layout from './components/layout/Layout';
--- src/components/layout/Meta.tsx
+++ src/components/layout/Meta.tsx
@@ -4,14 +4,12 @@ import { Helmet } from 'react-helmet';
 const Meta = ({
   title = 'Bread Toolkit',
   description = 'Online tools to help your baking journey',
-  keywords = 'bread, loaf, sourdough, toolbox, calculator, baking, hydratation',
 }) => {
   return (
     <Helmet>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="description" content={description} />
-      <meta name="keywords" content={keywords} />
     </Helmet>
   );
 };
