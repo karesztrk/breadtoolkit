ce03fc565c2e38f5ddd0ad846dab2895ea598c9c
Károly Török <torok.karoly.krisztian@gmail.com>
2020-12-13 14:03:02 +0100

    Basic layout unit tests

--- components/layout/Footer.test.tsx
+++ components/layout/Footer.test.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Footer from './Footer';
 
-jest.mock('next-localization', () => ({
-  useI18n: jest.fn().mockReturnValue({
-    t: (key: string) => key,
-  }),
-}));
-
 describe('Footer', () => {
   it('uses Bootstrap component class', () => {
     const { container } = render(<Footer />);
--- setupTests.js
+++ setupTests.js
@@ -1,13 +1,25 @@
 import '@testing-library/jest-dom/extend-expect';
 
+jest.mock('next-localization', () => ({
+  useI18n: jest.fn().mockReturnValue({
+    t: (key) => key,
+  }),
+}));
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn().mockReturnValue({
+    locale: {},
+  }),
+}));
+
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
   value: jest.fn().mockImplementation((query) => ({
     matches: false,
     media: query,
     onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
     addEventListener: jest.fn(),
     removeEventListener: jest.fn(),
     dispatchEvent: jest.fn(),
