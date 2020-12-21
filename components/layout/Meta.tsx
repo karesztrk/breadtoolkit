a304a9ff42bfd50a64e1af25aaca3c82835c6826
Károly Török <torok.karoly.krisztian@gmail.com>
2020-12-21 19:11:30 +0100

    PWA supports

--- components/layout/Meta.tsx
+++ components/layout/Meta.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import Head from 'next/head';
+import { useI18n } from 'next-localization';
 
 interface MetaProps {
   title?: string;
@@ -14,12 +15,48 @@ const Meta: FC<MetaProps> = ({
   description,
   keywords,
 }) => {
+  const { t } = useI18n();
+  const desc = description || t('meta.description');
+  const kywrds = keywords || t('meta.keywords');
   const headTitle = subtitle ? `${title} - ${subtitle}` : title;
   return (
     <Head>
       <title>{headTitle}</title>
-      {description && <meta name="description" content={description} />}
-      {keywords && <meta name="keywords" content={keywords} />}
+      <meta name="description" content={desc} />
+      <meta name="keywords" content={kywrds} />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="apple-mobile-web-app-title" content={title} />
+      <meta name="application-name" content={title} />
+
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content={title} />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="msapplication-square70x70logo" content="/logo192.png" />
+      <meta name="msapplication-square150x150logo" content="/logo192.png" />
+      <meta name="msapplication-square310x310logo" content="/logo512.png" />
+      <meta name="msapplication-TileColor" content="#3B3437" />
+      <meta name="msapplication-tap-highlight" content="no" />
+      <meta name="theme-color" content="#3B3437" />
+
+      <link rel="apple-touch-icon" sizes="192x192" href="/logo192.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/logo16.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/logo32.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/logo192.png" />
+      <link rel="icon" type="image/png" sizes="512x512" href="/logo512.png" />
+      <link rel="manifest" href="/static/manifest.json" />
+
+      <link rel="shortcut icon" href="/favicon.ico" />
+
+      <meta name="twitter:card" content={title} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content={title} />
     </Head>
   );
 };
--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
   // Target must be serverless
   target: 'serverless',
   i18n: {
     locales: ['en', 'hu'],
     defaultLocale: 'en',
   },
-};
+  pwa: {
+    dest: 'public',
+  },
+});
