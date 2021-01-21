7d6af5c759f06ea43d98a5aec25e95409efdd80f
Károly Török <torok.karoly.krisztian@gmail.com>
2021-01-21 19:52:09 +0100

    Website icon and Kofi animation

--- components/layout/Footer.test.tsx
+++ components/layout/Footer.test.tsx
@@ -3,8 +3,8 @@ import { render } from '@testing-library/react';
 import Footer from './Footer';
 
 describe('Footer', () => {
-  it('uses Bootstrap component class', () => {
+  it('has all the footer links', () => {
     const { container } = render(<Footer />);
-    expect(container.querySelectorAll('a')).toHaveLength(8);
+    expect(container.querySelectorAll('a')).toHaveLength(9);
   });
 });
--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -21,6 +21,8 @@ import { useRouter } from 'next/router';
 import en from '@/locales/en';
 import hu from '@/locales/hu';
 import KofiIcon from '../icons/KofiIcon';
+import { motion } from 'framer-motion';
+import WebIcon from '../icons/WebIcon';
 
 const Footer = () => {
   const { locale } = useRouter();
@@ -39,6 +41,7 @@ const Footer = () => {
             alignItems="center"
             py="2"
             direction={direction}
+            transition="color 150ms ease"
           >
             <Text
               flex={1}
@@ -61,16 +64,25 @@ const Footer = () => {
               spacing={4}
               lineHeight={4}
             >
-              <a href="https://ko-fi.com/K3K53CVET" target="_blank">
-                <KofiIcon color="#434B57" hoverColor="#29ABE0" />
+              <a
+                href="https://karolytorok.netlify.app/"
+                aria-label="Website"
+                target="_blank"
+              >
+                <WebIcon color="brand.500" hoverColor={hoverColor} />
               </a>
               <a
                 href="https://www.facebook.com/karoly.torok.9/"
                 aria-label="Facebook"
+                target="_blank"
               >
                 <FacebookIcon color="brand.500" hoverColor={hoverColor} />
               </a>
-              <a href="https://github.com/karesztrk" aria-label="Github">
+              <a
+                href="https://github.com/karesztrk"
+                aria-label="Github"
+                target="_blank"
+              >
                 <GitHubIcon color="brand.500" hoverColor={hoverColor} />
               </a>
               <a
@@ -82,21 +94,40 @@ const Footer = () => {
               <a
                 href="https://www.instagram.com/karesztrk/"
                 aria-label="Instagram"
+                target="_blank"
               >
                 <InstagramIcon color="brand.500" hoverColor={hoverColor} />
               </a>
               <a
                 href="https://www.linkedin.com/in/k%C3%A1roly-t%C3%B6r%C3%B6k-17541b80/"
                 aria-label="LinkedIn"
+                target="_blank"
               >
                 <LinkedInIcon color="brand.500" hoverColor={hoverColor} />
               </a>
               <a
                 href="https://www.youtube.com/user/r0nan87"
                 aria-label="Youtube"
+                target="_blank"
               >
                 <YouTubeIcon color="brand.500" hoverColor={hoverColor} />
               </a>
+              <motion.a
+                href="https://ko-fi.com/K3K53CVET"
+                target="_blank"
+                initial={{ y: 0, rotate: 0 }}
+                animate={{
+                  y: [0, -2, -2, -2, -2, 0],
+                  rotate: [0, 0, 15, -15, 0, 0],
+                }}
+                transition={{
+                  times: [0, 0.4, 0.5, 0.7, 0.8, 1],
+                  loop: Infinity,
+                  repeatDelay: 10,
+                }}
+              >
+                <KofiIcon color="#434B57" hoverColor="#29ABE0" />
+              </motion.a>
             </HStack>
           </Flex>
         </Container>
