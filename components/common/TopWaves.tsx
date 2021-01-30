6b6570c574990f722ca071cd9be0f28becbddb0e
Károly Török <torok.karoly.krisztian@gmail.com>
2021-01-30 18:40:46 +0100

    Animated top wave

--- components/common/TopWaves.tsx
+++ components/common/TopWaves.tsx
@@ -1,7 +1,10 @@
 import React, { FC } from 'react';
 import { Box, ChakraProps } from '@chakra-ui/react';
+import { usePrefersReducedMotion } from '@chakra-ui/react';
 
 const TopWaves: FC<ChakraProps> = (props: ChakraProps) => {
+  const prefersReducedMotion = usePrefersReducedMotion();
+  console.log(prefersReducedMotion);
   return (
     <Box
       overflow="hidden"
@@ -14,16 +17,66 @@ const TopWaves: FC<ChakraProps> = (props: ChakraProps) => {
       {...props}
     >
       <svg
-        viewBox="0 0 1200 120"
+        viewBox="0 0 1200 200"
         preserveAspectRatio="none"
         height="100%"
         width="calc(100% + 1.3px)"
         color="inherit"
       >
         <path
-          d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+          d="M 985.66 172.83 C 906.67 152 823.78 111 743.84 94.19 c -82.26 -17.34 -168.06 -16.33 -250.45 0.39 c -57.84 11.73 -114 31.07 -172 41.86 A 600.21 600.21 0 0 1 0 107.35 V 200 H 1200 V 175.8 C 1132.19 198.92 1055.71 191.31 985.66 172.83 Z"
           fill="currentColor"
-        ></path>
+        >
+          {!prefersReducedMotion && (
+            <animate
+              repeatCount="indefinite"
+              fill="currentColor"
+              attributeName="d"
+              dur="12s"
+              values="
+            M-150 100 
+            C 473,283
+              822,-40
+              1350,100 
+
+            V 359 
+            H 0 
+            V 67 
+            Z; 
+
+            M-150 100
+            C 473,-40
+              1222,283
+              1350,100 
+
+            V 359 
+            H 0 
+            V 67 
+            Z; 
+
+            M-150 100
+            C 973,260
+              1722,-53
+              1350,100 
+
+            V 359 
+            H 0 
+            V 67 
+            Z; 
+
+            M-150 100
+            C 473,283
+              822,-40
+              1350,100 
+
+            V 359 
+            H 0 
+            V 67 
+            Z
+            "
+            />
+          )}
+        </path>
       </svg>
     </Box>
   );
