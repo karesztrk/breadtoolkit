ed81ad1561cb91c866886c7b237ed20122803e42
Károly Török <torok.karoly.krisztian@gmail.com>
2026-01-05 18:32:15 +0100

    Recipes page

--- src/util/recipes.util.js
+++ src/util/recipes.util.js
@@ -1,8 +1,19 @@
 /**
+ * Format a recipe title.
  * @param {string} id
  * @return {string}
  */
 export const formatTitle = (id) => {
   const parts = id.split(".");
   return parts[0];
+};
+
+/**
+ * Format a recipe date.
+ * @param {Date} date
+ */
+export const formatDate = (date) => {
+  return new Intl.DateTimeFormat("en-GB", {
+    dateStyle: "full",
+  }).format(date);
 };
